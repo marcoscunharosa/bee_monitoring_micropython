17d1b48461c106f292f2f4fb23f960a4666ba083
LucasCastello <lucas.castello314@gmail.com>
2023-12-02 19:05:57 -0300

    refactored messenger

--- zmessenger.py
+++ zmessenger.py
@@ -3,12 +3,9 @@ import json
 
 def send_messages(ip, port, messages):
     try:
-        # Create a socket object
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            # Connect to the server (IP address and port)
             s.connect((ip, port))
 
-            # Send the first message
             s.sendall(messages[0].encode('utf-8'))
             print(f"Message 1 sent: {messages[0]}")
 
@@ -21,24 +18,24 @@ def send_messages(ip, port, messages):
             # print(f"Message 2 sent: {messages[1]}")
 
             # Receive data from the server (optional)
-            data = s.recv(1024)
+            data = s.recv(2048)
             print(f"Received from server: {data.decode('utf-8')}")
 
     except Exception as e:
         print(f"Error: {e}")
 
 a = {
-    "id": "5d0cdb5d-fc1e-48ff-a69b-88aaa44e1er5",
+    "id": "salve o corinthians",
     "location": {
-        "longitude": -47.07645,
-        "latitude": -22.82653
+        "longitude": -59.3453,
+        "latitude": -77.82653
     },
-    "name": "New Device 2",
+    "name": "Unicamp",
     "frequencyOfSavingData": {
-        "timeUnit": "MINUTES",
-        "timeValue": 15
+        "timeUnit": "SECONDS",
+        "timeValue": 1
     }
 }
 message2 = json.dumps(a)
-send_messages('10.0.0.103',80,['GET /device HTTP/1.1', message2])
-# ?until={5}
+message1 = 'GET /device HTTP/1.1\r\nuser-agent: Dart/3.1 (dart:io)\r\ncontent-type: application/json; charset=UTF-8\r\naccept-encoding: gzip\r\nhost: 10.0.0.103\r\n\r\n'
+send_messages('10.0.0.103',80,[message1, message2])
